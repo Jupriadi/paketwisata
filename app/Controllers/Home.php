1d889bf674e6ec7bdb9bdcfdb7e4a2a42a146121
Jupriadi <jpashter@gmail.com>
2021-08-03 09:00:52 +0800

    fitst commit

---
+++ app/Controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+	public function index()
+	{
+		$data = [
+			'judul' => 'SDIT Said Alamin',
+		];
+		return view('/pages/user/index',$data);
+	}
+}
---
+++ app/Views/templates/components/sidebar.php
@@ -0,0 +1,249 @@
+<div id="sidebar" class="active">
+    <div class="sidebar-wrapper active">
+        <div class="sidebar-header">
+            <div class="d-flex justify-content-between">
+                <div class="logo" style="width:100%">
+                    <a href="/panel">
+                        <img src="/assets/img/saidalamin.png" alt="Logo" style="width:100%;height:90%" >
+                    </a>
+                </div>
+                <div class="toggler">
+                    <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-menu">
+            <ul class="menu">
+                <li  class="sidebar-title">Menu</li>
+                <li class="sidebar-item" id="profsek"  >
+                    <a href="/panel/profsek" class='sidebar-link'>
+                        <i class="bi bi-hexagon-fill"></i>
+                        <span>Profil</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-title">Main Menu</li>
+
+                
+
+                <li class="sidebar-item  " id="guru">
+                    <a href="/guru" class='sidebar-link'>
+                        <i class="bi bi-cloud-haze2-fill"></i>
+                        <span>Destinasi Wisata</span>
+                    </a>
+                </li>
+                <li class="sidebar-item  " id="siswa">
+                    <a href="/siswa" class='sidebar-link'>
+                        <i class="bi bi-circle-square"></i>
+                        <span>Paket Wisata</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item" id="mapel">
+                    <a href="/mapel" class='sidebar-link'>
+                        <i class="bi bi-clipboard-check"></i>
+                        <span>Booking List</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  has-sub" id="surat">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-pen-fill"></i>
+                        <span>Surat Menyurat</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href="/surat/keterangansiswa">Keterangan Siswa</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item" id="slide">
+                    <a href="/tabungan" class='sidebar-link'>
+                        <i class="bi bi-grid-1x2-fill"></i>
+                        <span>Tabungan Siswa</span>
+                    </a>
+                </li>
+                <li class="sidebar-item" id="slide">
+                    <a href="/panel/slide" class='sidebar-link'>
+                        <i class="bi bi-grid-1x2-fill"></i>
+                        <span>Slide Show</span>
+                    </a>
+                </li>
+
+
+                <li class="sidebar-item  ">
+                    <a href="table-datatable.html" class='sidebar-link'>
+                        <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                        <span>Datatable</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-title">Extra UI</li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-pentagon-fill"></i>
+                        <span>Widgets</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href="ui-widgets-chatbox.html">Chatbox</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="ui-widgets-pricing.html">Pricing</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="ui-widgets-todolist.html">To-do List</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-egg-fill"></i>
+                        <span>Icons</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href="ui-icons-bootstrap-icons.html">Bootstrap Icons </a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="ui-icons-fontawesome.html">Fontawesome</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="ui-icons-dripicons.html">Dripicons</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-bar-chart-fill"></i>
+                        <span>Charts</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href="ui-chart-chartjs.html">ChartJS</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="ui-chart-apexcharts.html">Apexcharts</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="ui-file-uploader.html" class='sidebar-link'>
+                        <i class="bi bi-cloud-arrow-up-fill"></i>
+                        <span>File Uploader</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-map-fill"></i>
+                        <span>Maps</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href="ui-map-google-map.html">Google Map</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="ui-map-jsvectormap.html">JS Vector Map</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sidebar-title">Pages</li>
+
+                <li class="sidebar-item  ">
+                    <a href="application-email.html" class='sidebar-link'>
+                        <i class="bi bi-envelope-fill"></i>
+                        <span>Email Application</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="application-chat.html" class='sidebar-link'>
+                        <i class="bi bi-chat-dots-fill"></i>
+                        <span>Chat Application</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="application-gallery.html" class='sidebar-link'>
+                        <i class="bi bi-image-fill"></i>
+                        <span>Photo Gallery</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="application-checkout.html" class='sidebar-link'>
+                        <i class="bi bi-basket-fill"></i>
+                        <span>Checkout Page</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-person-badge-fill"></i>
+                        <span>Authentication</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href="auth-login.html">Login</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="auth-register.html">Register</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="auth-forgot-password.html">Forgot Password</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sidebar-item  has-sub">
+                    <a href="#" class='sidebar-link'>
+                        <i class="bi bi-x-octagon-fill"></i>
+                        <span>Errors</span>
+                    </a>
+                    <ul class="submenu ">
+                        <li class="submenu-item ">
+                            <a href="error-403.html">403</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="error-404.html">404</a>
+                        </li>
+                        <li class="submenu-item ">
+                            <a href="error-500.html">500</a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="sidebar-title">Raise Support</li>
+
+                <li class="sidebar-item  ">
+                    <a href="https://zuramai.github.io/mazer/docs" class='sidebar-link'>
+                        <i class="bi bi-life-preserver"></i>
+                        <span>Documentation</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="https://github.com/zuramai/mazer/blob/main/CONTRIBUTING.md" class='sidebar-link'>
+                        <i class="bi bi-puzzle"></i>
+                        <span>Contribute</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item  ">
+                    <a href="https://github.com/zuramai/mazer#donate" class='sidebar-link'>
+                        <i class="bi bi-cash"></i>
+                        <span>Donate</span>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        <button class="sidebar-toggler btn x"><i data-feather="x"></i></button>
+    </div>
+</div>
